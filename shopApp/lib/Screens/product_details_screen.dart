4b57ceceafd994fc91a37cf7a8ef719d11c50a82
Vishnu Anand <vishnuanand2000@gmail.com>
2020-08-03 23:47:05 +0530

    Added listeners

--- shopApp/lib/Screens/product_details_screen.dart
+++ shopApp/lib/Screens/product_details_screen.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../Provider/products_provider.dart';
 
 class DetailsScreen extends StatelessWidget {
-
   static const routeName = '/product-details';
 
   @override
   Widget build(BuildContext context) {
-
     final productId = ModalRoute.of(context).settings.arguments as String;
-
+    final loadedProduct = Provider.of<Products>(
+      context,
+      listen: false, //To make the widget not rebuild unnecessarily
+    ).findById(productId);
     return Scaffold(
       appBar: AppBar(
-        title: Text(productId),
+        title: Text(loadedProduct.title),
       ),
       body: Text("Details page"),
     );
