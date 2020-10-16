82e6615061b0703d4e8b561f12bf2056a93b8308
Vishnu Anand <vishnuanand2000@gmail.com>
2020-10-16 22:11:05 +0530

    Made favorite button functional

--- shopApp/lib/Screens/products_overview_screen.dart
+++ shopApp/lib/Screens/products_overview_screen.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
 
-import 'package:shopApp/Widgets/products_grid.dart';
+import '../widgets/products_grid.dart';
 
-class ProductsOverView extends StatelessWidget {
+class ProductsOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("MyShop"),
+        title: Text('MyShop'),
       ),
-      body: new ProductsGrid(),
+      body: ProductsGrid(),
     );
   }
 }
-
-
--- shopApp/lib/Widgets/product_item.dart
+++ shopApp/lib/Widgets/product_item.dart
@@ -1,51 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-import '../Screens/product_details_screen.dart';
+import '../screens/product_detail_screen.dart';
+import '../providers/product.dart';
 
 class ProductItem extends StatelessWidget {
-  final String id;
-  final String title;
-  final String imageUrl;
+  // final String id;
+  // final String title;
+  // final String imageUrl;
 
-//Constructor of the class
-  ProductItem(
-    this.id,
-    this.title,
-    this.imageUrl,
-  );
+  // ProductItem(this.id, this.title, this.imageUrl);
 
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context, listen: false);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         child: GestureDetector(
           onTap: () {
             Navigator.of(context).pushNamed(
-              DetailsScreen.routeName,
-              arguments: id, //Pushing id so that information can be fetched in a new page.
+              ProductDetailScreen.routeName,
+              arguments: product.id,
             );
           },
           child: Image.network(
-            imageUrl,
+            product.imageUrl,
             fit: BoxFit.cover,
           ),
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(
-            icon: Icon(Icons.favorite),
-            color: Theme.of(context).accentColor,
-            onPressed: () {},
+          leading: Consumer<Product>(
+            builder: (ctx, product, child) => IconButton(
+                  icon: Icon(
+                    product.isFavorite ? Icons.favorite : Icons.favorite_border,
+                  ),
+                  color: Theme.of(context).accentColor,
+                  onPressed: () {
+                    product.toggleFavoriteStatus();
+                  },
+                ),
           ),
           title: Text(
-            title,
+            product.title,
             textAlign: TextAlign.center,
           ),
           trailing: IconButton(
-            icon: Icon(Icons.shopping_cart),
-            color: Theme.of(context).accentColor,
+            icon: Icon(
+              Icons.shopping_cart,
+            ),
             onPressed: () {},
+            color: Theme.of(context).accentColor,
           ),
         ),
       ),
