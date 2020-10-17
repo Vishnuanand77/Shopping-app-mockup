badd8461842379b197bcc04e6ed03ba9663bd2c8
Vishnu Anand <vishnuanand2000@gmail.com>
2020-10-17 15:33:33 +0530

    Implemented add to cart button and badge

--- shopApp/lib/Screens/products_overview_screen.dart
+++ shopApp/lib/Screens/products_overview_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopApp/providers/cart.dart';
 
 import '../widgets/products_grid.dart';
+import '../widgets/badge.dart';
 
 enum FilterOptions {
   Favorites,
@@ -47,6 +50,18 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
               ),
             ],
           ),
+          Consumer<Cart>(
+            builder: (_, cart, ch) => Badge(
+              child: ch,
+              value: cart.itemCount.toString(),
+            ),
+            child: IconButton(
+                icon: Icon(
+                  Icons.shopping_cart,
+                ),
+                onPressed: () {},
+              ),
+          )
         ],
       ),
       body: ProductsGrid(_showFavoritesOnly),
